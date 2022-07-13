285bd9b1ba844c6d1824eee7ae6fc5ce945168fd
johnpaulavion14 <johnpaul.bocatija.14@gmail.com>
2022-07-13 16:12:09 +0800

    update

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -2,16 +2,6 @@ import { FC, useState } from 'react'
 import Content from './Content'
 import Sidebar from './Sidebar'
 import { Channel, User } from '../Types'
-<<<<<<< HEAD
-<<<<<<< HEAD
-import ChannelList from './ChannelList'
-=======
-import DirectMessage from './DirectMessage'
-import DMcontent from './DMcontent'
->>>>>>> direct-message
-=======
-
->>>>>>> 95dbb127341a82f9d4558e153daf69735c122b04
 
 interface Props {
     activeUser: User
@@ -23,54 +13,10 @@ const Dashboard: FC<Props> = ({ activeUser, setActiveUser }) => {
     const [channelName, setChannelName] = useState<string>('')
     const [channelID, setChannelID] = useState<number | null>(null)
     
-    // const [userID, setUserID] = useState<number | null>(null)
-    // const [activeUserName, setActiveUserNames] = useState<Channel[]>([])
-
-
 
     return (
-<<<<<<< HEAD
-<<<<<<< HEAD
-        <div className="main-container">
-            <div className='dashboard'>
-                <div className='sidebar'>
-                    <div className="main-content">
-                        <div className='main-top'>
-                            <h3>User: #{activeUser.data!.id}</h3>
-                        </div>
-                        <div className='main-mid'>
-                            <ChannelList activeUser={activeUser} activeUserChannels={activeUserChannels} setActiveUserChannels={setActiveUserChannels} setChannelName={setChannelName} setChannelID={setChannelID} channelName={channelName} channelID={channelID} />
-                        </div>
-                        <div className='main-bot'>
-                            <button onClick={logout} className='small-btn'>Logout</button>
-                        </div>
-                    </div>
-                </div>
-                <div className='content'>
-                    <Content activeUser={activeUser} setChannelName={setChannelName} channelName={channelName} channelID={channelID} />
-                </div>
-=======
-        <div className='flex'>
-            <div>
-                <div>Welcome to Avion Chat</div>
-                <div>Your user id is: #{activeUser.data!.id}</div>
-                <AddChannel activeUser={activeUser} activeUserChannels={activeUserChannels} setActiveUserChannels={setActiveUserChannels} setChannelName={setChannelName} setChannelID={setChannelID} channelName={channelName} channelID={channelID} />
-                <button onClick={logUsers}>console</button>
-                <button onClick={viewChannels}>View</button>
-                <DirectMessage activeUser={activeUser} />
-                <div>
-                    <button onClick={logout}>Logout</button>
-                </div>
-            </div>
-            <div>
-                <Content activeUser={activeUser} activeUserChannels={activeUserChannels} setActiveUserChannels={setActiveUserChannels} setChannelName={setChannelName} setChannelID={setChannelID} channelName={channelName} channelID={channelID} />
-                <DMcontent activeUser={activeUser} activeUserChannels={activeUserChannels} setActiveUserChannels={setActiveUserChannels} setChannelName={setChannelName} setChannelID={setChannelID} channelName={channelName} channelID={channelID} />
-            </div>
->>>>>>> direct-message
-=======
         <main className='flex justify-center items-center h-screen w-screen'>
             <Sidebar activeUser={activeUser} setActiveUser={setActiveUser} setChannelName={setChannelName} channelName={channelName} channelID={channelID} setChannelID={setChannelID} activeUserChannels={activeUserChannels} setActiveUserChannels={setActiveUserChannels} />
->>>>>>> 95dbb127341a82f9d4558e153daf69735c122b04
 
             <Content activeUser={activeUser} setChannelName={setChannelName} channelName={channelName} channelID={channelID} />
         </main>
--- src/components/DirectMessages.tsx
+++ src/components/DirectMessages.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC,useState } from 'react'
 import { User } from '../Types'
 
 interface Props {
@@ -7,26 +7,94 @@ interface Props {
 
 const DirectMessages: FC<Props> = ({ activeUser }) => {
 
+    const [userIdArrays, setUserIdArrays] = useState<Array<any>>([])
+
     async function getUserDMs() {
-        let activeUserID
+        let activeUserID:number
         if (activeUser.data !== undefined) {
             activeUserID = activeUser.data.id
         }
-        const url = `http://206.189.91.54/api/v1/messages?receiver_id=${activeUserID}&receiver_class=User`
-        const response = await fetch(url,
+        const urls = "http://206.189.91.54/api/v1/users"
+        const responses = await fetch(urls,
             {
                 method: "GET",
-                headers: activeUser.headers
+                headers: activeUser.headers,
             })
-        const data = await response.json()
-        if (response.ok) {
-            console.log(data)
-            console.log(activeUserID)
-        } else console.log(response)
+        const datas = await responses.json()
+        let database: Array<any>=[]
+        // const aaaaray:Array<number> = [2216,2222,2215]
+        datas.data.forEach((element: any) => {
+            async function sendDirectMessage() {
+             const responses = await fetch(`http://206.189.91.54/api/v1/messages?receiver_id=${element.id}&receiver_class=User`,
+            {
+                method: "GET",
+                headers: activeUser.headers,
+            })
+            const userData = await responses.json()
+            const findData = userData.data.some((x:{sender:{id:number};receiver:{id:number}})=> x.sender.id === activeUserID || x.receiver.id === activeUserID)
+            if(findData){
+                const mapUserData = userData.data.map((element: { body: any }) => {
+                   return{
+                    message:element.body,
+                    time:10
+                   }
+                }) 
+            
+                database.push({[element.id]:mapUserData})
+            }
+            
+            }
+            sendDirectMessage()
+        })
+        setUserIdArrays(database)
+
+       
+        // const url = `http://206.189.91.54/api/v1/messages?receiver_id=${activeUserID}&receiver_class=User`
+        // const response = await fetch(url,
+        //     {
+        //         method: "GET",
+        //         headers: activeUser.headers
+        //     })
+        // const data = await response.json()
+        // if (response.ok) {
+        //     console.log(data)
+        //     console.log(activeUserID)
+        // } else console.log(response)
     }
+    // userIdArrays.forEach((element: any) => {
+
+    //     async function getUserDMs() {
+    //         const response = await fetch(`http://206.189.91.54/api/v1/messages?receiver_id=${element}&receiver_class=User`,
+    //             {
+    //                 method: "GET",
+    //                 headers: activeUser.headers
+    //             })
+    //         const data = await response.json()
+    //        console.log(data.data.body)
+    //     }
+    // })
+    const listDMs =  userIdArrays.length === 0
+    ?
+    <div>
+        <span>User has no DM</span>
+    </div>
+    :
+    userIdArrays.map(element => {
+        let userID = Object.keys(element)[0]
+        return (
+            <><button>
+                {userID}
+            </button><br /></>
+        )
+    })
+    console.log(userIdArrays)
+
     return (
         <div>
             <button onClick={getUserDMs}>Log DMS</button>
+            <div className='px-2'>
+                {listDMs}
+            </div>
         </div>
     )
 }
